aea8c4e406a04d164b63aa98307f805aef0a09ca
Lei jie Xu wu <lxu-wu@fu-r6-p4.s19.be>
2022-08-24 02:10:29 +0200

    v1

---
+++ cgi.hpp
@@ -0,0 +1,14 @@
+#ifndef CGI_HPP
+# define CGI_HPP
+
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <stdlib.h>
+#include <climits>
+#include <sys/stat.h>
+
+
+
+#endif
